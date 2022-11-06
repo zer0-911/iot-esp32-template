b60fa05d3378980e55a9569dbac85801703268ad
zer0-911 <mohiqbalf5@gmail.com>
2022-11-06 15:35:24 +0700

    feat: With HTTP

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,59 @@
 #include <Arduino.h>
-
-#define LED 2
-
+#include <WiFi.h>
+#include <HTTPClient.h>
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+  uint8_t temprature_sens_read();
+#ifdef __cplusplus
+}
+#endif
+uint8_t temprature_sens_read();
+// Masukan ssid dan password dari router
+const char *ssid = "Hiya";
+const char *password = "1234567890";
+// Alamat IP server dengan portnya
+unsigned long lastTime = 0;
+unsigned long timerDelay = 1000;
 void setup()
 {
-  // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(LED, OUTPUT);
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
 }
-
 void loop()
 {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  Serial.println("LED is on");
-  delay(1000);
-  digitalWrite(LED, LOW);
-  Serial.println("LED is off");
-  delay(1000);
+  if ((millis() - lastTime) > timerDelay)
+  {
+    // cek koneksi
+    if (WiFi.status() == WL_CONNECTED)
+    {
+      WiFiClient client;
+      HTTPClient http;
+      float temp = (temprature_sens_read() - 32) / 1.8;
+      char serverName[100];
+      sprintf(serverName, "http://192.168.204.93:5000/esp?temperature=%3.2f",
+              temp); // Setting IP Address and Port as per your server
+      http.begin(client, serverName);
+      // kirimkan HTTP POST
+      int httpResponseCode = http.POST("");
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      http.end();
+    }
+    else
+    {
+      Serial.println("WiFi Disconnected");
+    }
+    lastTime = millis();
+  }
 }
